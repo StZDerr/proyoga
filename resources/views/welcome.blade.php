622aab3411726736cd4ab660bd18b4e82110dbe3
StZDer <Sasha-vasilchenko-1999@mail.ru>
2025-11-10 17:05:53 +0300

    Скрыт у Фотогалерии Lorem подробнее

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -566,7 +566,7 @@
                 <div class="d-flex align-items-center justify-content-center">
                     <div class="text text-center">
                         <h2 class="section-title">Фотогалерея</h2>
-                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                        {{-- <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span> --}}
                     </div>
                 </div>
                 <div class="container mt-5">
